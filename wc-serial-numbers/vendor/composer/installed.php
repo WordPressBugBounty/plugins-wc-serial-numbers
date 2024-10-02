d31623a573080acb7c493d0d89875950d017ca6c
Sean Murphy <sean@iamseanmurphy.com>
2024-10-02 04:46:45 +0000

    2.0.5

---
+++ wc-serial-numbers/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pluginever/wc-serial-numbers',
+        'pretty_version' => 'v2.0.5',
+        'version' => '2.0.5.0',
+        'reference' => 'c9e700f489c223fa0581c7ac220233538e747aca',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'pluginever/wc-serial-numbers' => array(
+            'pretty_version' => 'v2.0.5',
+            'version' => '2.0.5.0',
+            'reference' => 'c9e700f489c223fa0581c7ac220233538e747aca',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
